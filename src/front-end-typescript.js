600a9957be98a77aa28672c9d3f5d70d5b5da6bb
Jonathan Toung <jtoung@grailbio.com>
2019-04-05 15:06:58 +0000

    ui/eslint-config-grail: add front-end-typescript extension
    Reviewers: O13 UI Common, jsingh, jzhao
    
    Reviewed By: O13 UI Common, jsingh, jzhao
    
    Differential Revision: https://phabricator.grailbio.com/D27979
    
    fbshipit-source-id: 5b9ed6d

---
+++ src/front-end-typescript.js
@@ -0,0 +1,14 @@
+// @flow
+module.exports = {
+  extends: [
+    "@grail/eslint-config-grail/src/front-end.js",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/indent": ["error", 2],
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/explicit-member-accessibility": "off",
+  },
+};

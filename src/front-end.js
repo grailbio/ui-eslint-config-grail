8578c2706bff4d94194546a025c13a788ef6dde4
Jack Zhao <yzhao@grailbio.com>
2019-08-09 11:54:24 -0700

    lims/ui: upgrade packages
    Summary: react 16.9, create-react-app 3.1.
    
    Test Plan: UI
    
    Reviewers: jsingh, lkong, O14 Skynet UI, O13 UI Common, O12 LIMS UI, nsawas
    
    Reviewed By: jsingh, lkong, O14 Skynet UI, O13 UI Common, O12 LIMS UI
    
    Maniphest Tasks: T8000
    
    Differential Revision: https://phabricator.grailbio.com/D34718
    
    fbshipit-source-id: 23f16c0

--- src/front-end.js
+++ src/front-end.js
@@ -33,6 +33,7 @@ module.exports = {
       },
     ],
     "react/jsx-first-prop-new-line": [1, "multiline-multiprop"],
+    "react/jsx-props-no-spreading": "off",
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn",
   },
